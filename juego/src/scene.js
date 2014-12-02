9622e4b5242f963ba7387d8dbf20b9b8b1a806d5
Ameriko <jerez.americo@gmail.com>
2014-12-01 23:34:39 -0400

    Cambios a la página.

--- juego/src/scene.js
+++ juego/src/scene.js
@@ -138,6 +138,131 @@ Crafty.scene('Game1', function() {
 	// Play a ringing sound to indicate the start of the journey
 	Crafty.audio.play('ring');
  });
+ 
+Crafty.scene('Game2', function() {
+	// A 2D array to keep track of all occupied tiles
+	this.occupied = new Array(Game.map_grid.width);
+	for (var i = 0; i < Game.map_grid.width; i++) {
+		this.occupied[i] = new Array(Game.map_grid.height);
+		for (var y = 0; y < Game.map_grid.height; y++) {
+			this.occupied[i][y] = false;
+		}
+	}
+	 
+	// Place a border at every edge square on our grid of 16x16 tiles
+	for (var x = 0; x < Game.map_grid.width; x++) {
+		for (var y = 0; y < Game.map_grid.height; y++) {
+			var at_edge = x == 0 || x == Game.map_grid.width - 1 || y == 0 || y == Game.map_grid.height - 1;
+			 
+			if (at_edge) {
+				// Place a tree entity at the current tile
+				Crafty.e('Borde').at(x, y)
+				this.occupied[x][y] = true;
+			}// else if (Math.random() < 0.06 && !this.occupied[x][y]) {
+			// Place a bush entity at the current tile
+			//var bush_or_rock = (Math.random() > 0.3) ? 'Bush' : 'Rock'
+			//Crafty.e(bush_or_rock).at(x, y)
+			//this.occupied[x][y] = true;
+			//}
+		}
+	}
+	//place a border at 21x2:14 to separate the game area
+	 for (var i = 1; i < Game.map_grid.width - 1; i++){
+	 	Crafty.e('Borde').at(21,i)
+	 	this.occupied[21][i] = true;
+	 }
+	 //place the wires
+	 for(var i=2;i<20;i++){
+	 	//condiciones para horizontales
+	 	if(i>=2 && i<=6){
+	 		Crafty.e('Cable_hor').at(i,3);
+	 		Crafty.e('Cable_hor').at(i,5);
+	 		Crafty.e('Cable_hor').at(i,9);		
+	 		Crafty.e('Cable_hor').at(i,11);
+	 		this.occupied[i][3] = true;
+	 		this.occupied[i][5] = true;
+	 		this.occupied[i][9] = true;
+	 		this.occupied[i][11] = true;
+	 	}
+	 	if(i>=8 && i<=9){
+	 		Crafty.e('Cable_hor').at(i,4);
+	 		Crafty.e('Cable_hor').at(i,10);
+	 		this.occupied[i][4] = true;
+	 		this.occupied[i][10] = true;
+	 	}
+		if(i>=11 && i<=19){
+	 		Crafty.e('Cable_hor').at(i,4);
+	 		Crafty.e('Cable_hor').at(i,10);
+	 		this.occupied[i][4] = true;
+	 		this.occupied[i][10] = true;
+	 	}
+	 	if(i==11 || i==12){
+	 		Crafty.e('Cable_hor').at(i,6);
+	 		Crafty.e('Cable_hor').at(i,8);
+	 		this.occupied[i][6] = true;
+	 		this.occupied[i][8] = true;
+	 	}
+	 	if(i>=14 && i<=16){
+	 		Crafty.e('Cable_hor').at(i,7);
+	 		this.occupied[i][7] = true;
+	 	}
+	 	if(i>=18 && i<=19){
+	 		Crafty.e('Cable_hor').at(i,7);
+	 		this.occupied[i][7] = true;
+	 	}
+	 }
+	 Crafty.e('Cable_ver').at(10,5);
+	 this.occupied[10][5] = true;
+	 Crafty.e('Cable_ver').at(10,9);
+	 this.occupied[10][9] = true;
+	 Crafty.e('Cable_cod1').at(10,8);
+	 this.occupied[10][8] = true
+	 Crafty.e('Cable_cod4').at(10,6);
+	 this.occupied[10][6] = true;
+	 Crafty.e('Cable_tri2').at(10,4);
+	 this.occupied[10][4] = true;
+	 Crafty.e('Cable_tri1').at(10,10);
+	 this.occupied[10][10] = true;	 
+
+	 //place Bits
+	 Crafty.e('In1').at(1, 3)
+	 Crafty.e('In0').at(1, 5)
+	 Crafty.e('In1').at(1, 9)
+	 Crafty.e('In0').at(1, 11)
+	 Crafty.e('Out1').at(20, 4)
+	 Crafty.e('Out0').at(20, 7)
+	 Crafty.e('Out0').at(20, 10)
+	 
+	 //place the things
+	 var selector_chico = Crafty.e('Selector_chico').at(-1,-1);
+	 var selector_grande = Crafty.e('Selector_grande').at(-1,-1);
+	 var compuertas = [];
+	 compuertas.push(Crafty.e('Not').at(23,2).compuerta(23,2,this.occupied,selector_chico));
+	 compuertas.push(Crafty.e('Not').at(25,2).compuerta(25,2,this.occupied,selector_chico));
+
+	 compuertas.push(Crafty.e('And').at(23,4).compuerta(23,4,this.occupied,selector_grande));
+	 compuertas.push(Crafty.e('And').at(25,4).compuerta(25,4,this.occupied,selector_grande));
+	
+	 compuertas.push(Crafty.e('Or').at(23,8).compuerta(23,8,this.occupied,selector_grande));
+	 compuertas.push(Crafty.e('Or').at(25,8).compuerta(25,8,this.occupied,selector_grande));
+
+	 //condiciones para ganar. Cuidado con tener varias soluciones para el problema.
+	 var condiciones = [];
+	 condiciones[0]=[];
+	 condiciones[0].push(['not',17,7]);
+	 condiciones[0].push(['and',7,9]);
+	 condiciones[0].push(['or',7,3]);
+	 condiciones[0].push(['or',13,6]);
+	 
+
+	 var controlador=Crafty.e('Victoria').Victoria(compuertas, condiciones);
+	 //Crafty.addEvent(controlador, Crafty.stage.elem,'Revisar', controlador.revisar);
+
+
+
+	// Play a ringing sound to indicate the start of the journey
+	Crafty.audio.play('ring');
+ });
 // Loading scene
 // -------------
 // Handles the loading of binary assets such as images and audio files
@@ -211,6 +336,9 @@ ring: ['assets/candy_dish_lid.mp3', 'assets/candy_dish_lid.ogg', 'assets/candy_d
 });
  
 // Now that our sprites are ready to draw, start the game
-Crafty.scene('Game1' );
+if(Crafty.dinoRex==1)
+Crafty.scene('Game1');
+else
+Crafty.scene('Game2');
 });
 });
